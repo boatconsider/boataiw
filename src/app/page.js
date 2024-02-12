f696723689277def7a32b9a5a7e02b3764f2f5d9
boatconsider <boatzarock01@gmai.com>
2024-02-12 21:16:44 +0700

    d

--- src/app/page.js
+++ src/app/page.js
@@ -10,76 +10,58 @@ export default function Home() {
 
 
 
-  <div className="flex flex-row justify-center items-center flex-wrap ">
-  <div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
-    <source src="/13.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
+  <div className="flex flex-row justify-center items-center flex-wrap  ">
+
+  <div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0  ">
+<img src="/10.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
 </div>
 
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0  ">
+<img src="/11.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
+</div>
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0">
+<img src="/12.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
+</div>
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0">
+  <video  className="absolute inset-0 w-full h-full object-cover">
     <source src="/1.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
 
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0">
+  <video  className="absolute inset-0 w-full h-full object-cover">
     <source src="/2.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden max-[480px]:m-0 max-[820px]:m-0 ">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0 ">
+  <video  className="absolute inset-0 w-full h-full object-cover">
     <source src="/3.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
-    <source src="/4.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-</div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
-    <source src="/5.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-</div>
-
-
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden  max-[480px]:m-0 max-[820px]:m-0 ">
-<img src="/12.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
-</div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0">
+  <video  className="absolute inset-0 w-full h-full object-cover">
     <source src="/7.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0">
+<video  className="absolute inset-0 w-full h-full object-cover">
+    <source src="/13.mp4" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+</div>
+
+
+<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 rounded-full  max-[480px]:m-0 max-[820px]:m-0 ">
+<video  className="absolute inset-0 w-full h-full object-cover">
     <source src="/8.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 </div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden  max-[480px]:m-0 max-[820px]:m-0 ">
-  <video autoPlay loop className="absolute inset-0 w-full h-full object-cover">
-    <source src="/9.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-</div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
- 
-  <img src="/10.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
-   
-</div>
-<div className="relative w-[400px] h-[400px] bg-[#fff] mt-5 overflow-hidden ml-5 max-[480px]:m-0 max-[820px]:m-0">
-<img src="/11.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
-</div>
+
 
 
      
