46ccb96087597073f727f210bb748c204b43a35e
boatconsider <boatzarock01@gmai.com>
2024-02-13 19:44:46 +0700

    jjjjk

--- src/app/page.js
+++ src/app/page.js
@@ -6,25 +6,27 @@ export default function Home() {
 <div>
 <img src="https://cdn.wallpapersafari.com/77/64/OeTYUZ.jpg" alt="" className="w-full h-full object-cover" />
 </div>
-<div className="text-center text-5xl">
+<div className="text-center text-5xl mt-3 ">
   <h1 style={{ background: "-webkit-linear-gradient(45deg, #ff00ff, #ffffff)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }}> สุขสันต์วันวาเลนไทน์ไอ้อ้วง🩷🩷 </h1>
   <div className="flex justify-center items-center flex-col ">
   <p style={{ background: "-webkit-linear-gradient(45deg, #ff00ff, #ffffff)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }}>กด play จิอิอิ</p>
 <div>
   
-<audio controls autoPlay className="mt-3">
+<audio controls autoPlay className="mt-5">
     <source className="mt-3" src="/หมดนี้ให้เธอ.mp3" type="audio/mpeg" />
   </audio>  
 </div>
+
+
 </div>
 </div>
 
 
 
 
-  <div className="flex flex-row justify-center items-center flex-wrap  ">
+  <div className="flex flex-row justify-center items-center flex-wrap mt-6  ">
 
-  <div className="relative w-[400px] h-[400px] bg-[#fff] mt-7 overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0  ">
+  <div className="relative w-[400px] h-[400px] bg-[#fff]  overflow-hidden ml-5 rounded-full max-[480px]:m-0 max-[820px]:m-0  ">
 <img src="/10.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
 </div>
 
